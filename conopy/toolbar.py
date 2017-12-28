41aa919e9265cfe52c849d59c6e680f6bc9375af
sshmakov <sergey-shmakov@yandex.ru>
2017-12-28 23:35:41 +0300

    Key shortcuts for toolbar

--- conopy/toolbar.py
+++ conopy/toolbar.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
+import sys, re
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 import importlib
 
 class ToolBar(QToolBar):
@@ -11,24 +12,36 @@ class ToolBar(QToolBar):
         super(ToolBar, self).__init__(parent)
         ini = QSettings(iniFile, QSettings.IniFormat)
         ini.setIniCodec("utf-8")
+        rx = re.compile('([^(]+)\s*(?:\((.*)\))?')
         ini.beginGroup("Tools")
         for key in sorted(ini.childKeys()):
             v = ini.value(key)
-            title = v[0]
+            title = saveTitle = v[0]
             params = v[1:]
+            tkey = rx.findall(title)
+            keySeq = None            
+            if tkey and len(tkey) == 1 and len(tkey[0]) == 2 and len(tkey[0][1])>0:
+                title = tkey[0][0].lstrip().rstrip()
+                keySeq = QKeySequence(tkey[0][1])
             a = self.addAction(title)
             a.params = params
             a.triggered.connect(self.execAction)
+            if keySeq:
+                a.setShortcut(keySeq)
+            a.setToolTip(saveTitle)
         ini.endGroup()
 
     def execAction(self):
         try:
             params = self.sender().params
             module = importlib.import_module(params[0])
-            if len(params) < 2: func = "run()"
+            if len(params) < 2: func = "run(win)"
             else: func = params[1]
             win = self.focusTaskWindow()
-            exec("module.%s(win)" % func)
+            if func.find('(') < 0:
+                exec("module.%s(win)" % func)
+            else:
+                exec("module.%s" % func)
         except:
             print(str(sys.exc_info()[1]))
         return
