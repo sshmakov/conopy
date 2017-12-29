ad62866628b6cff569b500bca4f9baa5fbeab168
sshmakov <sergey-shmakov@yandex.ru>
2017-12-29 23:39:01 +0300

    Added field filter panel

--- conopy/sqlexecutor.py
+++ conopy/sqlexecutor.py
@@ -7,10 +7,9 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtSql import *
 if __package__ is None or __package__ == '':
     from executor import PyExecutor
-    import meshandler
-    import dbpool
+    import meshandler, dbpool, util
 else:
-    from . import (meshandler, dbpool)
+    from . import (meshandler, dbpool, util)
     from .executor import PyExecutor
 
 class QueryRunner(QThread):
@@ -26,6 +25,25 @@ class QueryRunner(QThread):
 class SqlExecutor(PyExecutor):
     def __init__(self, iniFile, parent=None):
         super().__init__(iniFile, parent)
+        self.view = None
+        self.proxyEnabled = True
+        self.createFilterPane()
+
+    def createFilterPane(self):
+        self.filterLay = QHBoxLayout()
+        self.btnLay.insertLayout(0,self.filterLay)
+        self.fieldChoice = QComboBox(self)
+        self.filterLay.addWidget(self.fieldChoice)
+        self.valueFind = QComboBox(self)
+        self.valueFind.setEditable(True)
+        self.valueFind.setMinimumWidth(200)
+        self.filterLay.addWidget(self.valueFind)
+        self.btnFilter = QPushButton('Filter',self)
+        self.btnFilter.setCheckable(True)
+        self.btnFilter.clicked.connect(self.filterClick)
+        self.filterLay.addWidget(self.btnFilter)
+        self.resetFieldChoice()
+        
 
     def loadIni(self, iniFile):
         super().loadIni(iniFile);
@@ -35,9 +53,7 @@ class SqlExecutor(PyExecutor):
         self.dbini = ini.value("DBConnect")
         if self.dbini == "this":
            self.dbini = iniFile
-        self.dbini = os.path.join(
-            os.path.split(
-                os.path.abspath(self.iniFile))[0], self.dbini)
+        self.dbini = util.nearFile(self.iniFile, self.dbini)
         ini.endGroup()
 
         ini.beginGroup("Run")
@@ -45,42 +61,13 @@ class SqlExecutor(PyExecutor):
             self.sql = ini.value("SQL")
         else:
             scriptFile = ini.value("SQLScript")
-            scriptFile = os.path.join(os.path.split(
-                os.path.abspath(self.iniFile))[0], scriptFile)
-            print("scriptFile:", scriptFile)
+            scriptFile = util.nearFile(self.iniFile, scriptFile)
+            # print("scriptFile:", scriptFile)
             f = QFile(scriptFile)
             f.open(QIODevice.ReadOnly)
             self.sql = str(f.readAll(),'utf-8-sig')
-        #print(bytes(self.sql,'utf-8'))
         ini.endGroup()
         
-
-    def createModel(self, parent=None):
-        return QSqlQueryModel(parent)
-
-    def showResult(self):
-        super().showResult()
-        err = self.query.lastError()
-        if err.type() != QSqlError.NoError:
-            self.bar.showMessage("Error {0} {1}".format(
-                err.number(), err.text()))
-        else:
-            self.bar.showMessage(
-                "Rows affected: {0}  Rows: {1}".format(
-                    self.query.numRowsAffected(), self.query.size() ))
-            res = self.query.result()
-            while res and self.query.isSelect():
-                w = self.createView()
-                w.sqlModel = self.createModel(w)
-                w.sqlModel.setQuery(self.query)
-                self.query.first()
-                w.proxyModel = QSortFilterProxyModel(w)
-                w.proxyModel.setSourceModel(w.sqlModel)
-                w.setModel(w.proxyModel)
-                self.resultLay.addWidget(w)
-                if not self.query.nextResult():
-                    break
-                res = self.query.result()
 
     def run(self):
         self.runBtn.setEnabled(False)
@@ -107,9 +94,85 @@ class SqlExecutor(PyExecutor):
             self.tr.finished.connect(self.showResult)
             self.tr.start();
 
+    def showResult(self):
+        super().showResult()
+        err = self.query.lastError()
+        if err.type() != QSqlError.NoError:
+            self.bar.showMessage("Error {0} {1}".format(
+                err.number(), err.text()))
+            return
+        res = self.query.result()
+        while res and self.query.isSelect():
+            self.createViewModel()
+            self.view.sqlModel.setQuery(self.query)
+            self.query.first()
+            if not self.query.nextResult():
+                break
+            res = self.query.result()
+        self.bar.showMessage(
+            "Rows affected: {0}  Rows: {1}".format(
+                self.query.numRowsAffected(), self.query.size() ))
+        self.resetFieldChoice()
+
+    def filterClick(self):
+        if self.btnFilter.isChecked():
+            self.view.proxyModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
+            self.view.proxyModel.setFilterKeyColumn(self.fieldChoice.currentIndex()-1)
+            self.view.proxyModel.setFilterRegExp(self.valueFind.currentText())
+        else:
+            self.view.proxyModel.setFilterRegExp("")
+
+    def resetFieldChoice(self):
+        cur = self.fieldChoice.currentText()
+        self.fieldChoice.clear()
+        self.fieldChoice.addItem("- Любое поле -")
+        model = self.view.model() if self.view else None
+        if model:
+            headers = [str(model.headerData(c,Qt.Horizontal)) for c in range(self.view.model().columnCount())]
+            self.fieldChoice.addItems(headers)
+        self.fieldChoice.setCurrentText(cur)
+        
+
+    def createModel(self, parent=None):
+##        if parent and parent.sqlModel:
+##            return parent.sqlModel
+        return QSqlQueryModel(parent)
+
+    def createProxy(self, parent=None):
+        if parent and parent.proxyModel:
+            return parent.proxyModel
+        return QSortFilterProxyModel(parent)
+
+    def clearResult(self):
+        self.bar.clearMessage()
+
+    def createViewModel(self):
+        if not self.view:
+            self.view = QTableView(self)
+            vh = self.view.verticalHeader()
+            vh.setDefaultSectionSize(19)
+            self.view.setSortingEnabled(True)
+            self.view.horizontalHeader().setSectionsMovable(True)
+            self.view.horizontalHeader().setSortIndicator(-1,Qt.AscendingOrder)
+            self.resultLay.addWidget(self.view)
+            self.view.sqlModel = None
+            self.view.proxyModel = None
+        if not self.view.sqlModel:
+            self.view.sqlModel = self.createModel(self.view)
+        if self.proxyEnabled:
+            if not self.view.proxyModel:
+                self.view.proxyModel = QSortFilterProxyModel(self.view)
+                self.view.proxyModel.setDynamicSortFilter(True)
+                self.view.proxyModel.setSortRole(Qt.EditRole)
+            self.view.proxyModel.setSourceModel(self.view.sqlModel)
+            self.view.setModel(self.view.proxyModel)
+        else:
+            self.view.setModel(self.view.sqlModel)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = SqlExecutor("../data/test-sqlite.ini")
+    ini = util.nearFile(__file__,"../data/sqlite/test-sqlite.ini")
+    ex = SqlExecutor(ini)
     ex.show()
 
     sys.exit(app.exec_())
