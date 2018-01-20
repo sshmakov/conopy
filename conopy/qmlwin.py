d42dd6b48f23c72433c97580fd6739ae8b88b6a2
sshmakov <sergey-shmakov@yandex.ru>
2018-01-20 11:21:47 +0300

    Fixed QML loading

--- conopy/qmlwin.py
+++ conopy/qmlwin.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtQuickWidgets import *
 import PyQt5
 import os
+import util
 
 
 class QmlWin(QQuickWidget):
@@ -17,8 +18,10 @@ class QmlWin(QQuickWidget):
         ini = QSettings(iniFile, QSettings.IniFormat)
         ini.setIniCodec("utf-8")
         ini.beginGroup("QML")
-        qml = ini.value("Source")
-        if qml != None:
+        self.qml = ini.value("Source")
+        if not self.qml is None:
+            self.qml = QUrl.fromUserInput(util.nearFile(iniFile, self.qml))
+            #print(self.qml)
             self.setSource(QUrl(qml))
         else:
             print("No source qml")
@@ -28,10 +31,12 @@ if __name__ == '__main__':
     import meshandler
     import sys
 
+    os.environ.putenv('QT_ANGLE_PLATFORM','warp') # d3d11, d3d9 and warp
+    os.environ.putenv('QT_OPENGL','software') # desktop, software, angle
     pyqt = os.path.dirname(PyQt5.__file__)
     QApplication.addLibraryPath(os.path.join(pyqt, "Qt", "plugins"))
 
     app = QApplication(sys.argv)
-    w = QmlWin("../dynamicview.ini")
+    w = QmlWin("../data/qml/dynamicview.ini")
     w.show()
     sys.exit(app.exec_())
